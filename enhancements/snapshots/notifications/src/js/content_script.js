4f2ba5238834f529ea9c7243973623c45259422d
ahadik <ahadik@us.ibm.com>
2016-02-29 11:38:19 -0600

    Basic informational notifications are complete.

--- enhancements/snapshots/notifications/src/js/content_script.js
+++ enhancements/snapshots/notifications/src/js/content_script.js
@@ -2,7 +2,31 @@
 Allowable types are alert-danger, alert-info, alert-success
 */
 
-function addAlert(alertContent, type){
+var container, alertWrapper;
+var attached = false;
+var info = [];
+var danger = [];
+var success = [];
+
+
+function createSingleInfoContent(volName){
+	return 'Your snapshot for Volume \''+volName+'\' is being created! You can find it in the Snapshots tab when it\'s complete.';
+}
+
+function createMultiInfoContent(numSnapshots){
+	return content = 'Your snapshots ('+numSnapshots+') are being created! You can find them in the Snapshots tab when they\'re complete.';
+}
+
+function appendAlertContent(alert, content){
+	var strong = document.createElement('strong');
+	var contentSpan = document.createElement('span');
+	contentSpan.appendChild(strong);
+	contentSpan.appendChild(document.createTextNode(content));
+	alert.appendChild(contentSpan);
+	return alert;
+}
+
+function createAlert(alertContent, type){
 	var alert = document.createElement('div');
 	alert.classList.add('alert');
 	alert.classList.add(type);
@@ -11,40 +35,102 @@ function addAlert(alertContent, type){
 	closeButton.classList.add('close');
 
 	var xSpan = document.createElement('span');
+	xSpan.setAttribute('aria-hidden', 'true');
 	xSpan.appendChild(document.createTextNode('x'));
 	closeButton.appendChild(xSpan);
 	alert.appendChild(closeButton);
-
-	var strong = document.createElement('strong');
-	var title = null;
-	if(type == 'alert-danger'){
-		title = 'Uh oh!';
-	}else if(type == 'alert-info'){
-		title = 'Heads up!'
-	}else if (type == 'alert-success'){
-		title = 'Way to go!'
-	}
-	strong.appendChild(document.createTextNode(title));
-	alert.appendChild(strong);
-
-	alert.appendChild(document.createTextNode(alertContent));
+	alert = appendAlertContent(alert, alertContent);
 	return alert;
 }
 
-if (parent === top){
-	window.onload = function(){
-		var container = document.querySelector('.data-row');
-		var alertWrapper = document.createElement('div');
-		alertWrapper.classList.add('alertWrapper');
-		alertWrapper.classList.add('inline');
-		container.appendChild(alertWrapper);
-		alertWrapper.appendChild(addAlert('This is a success!', 'alert-success'));
+function refreshAlerts(type, collection){
+	var currAlert = document.querySelector('.'+type);
+	currAlert.removeChild(currAlert.getElementsByTagName('span'));
+	if (collection.length > 1){
+		currAlert = appendAlertContent(currAlert, createMultiInfoContent(collection.length));
+	}else if (collection.length == 1){
+		currAlert = appendAlertContent(currAlert, createSingleInfoContent(getSelectedSnapshotName()));
 	}
 }
 
-/*
-<div class="alert alert-danger alert-dismissible" role="alert">
-	<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
-	<strong>Uh oh!</strong> Descriptive alert messages appears here.
-</div>
-*/
+function addAlert(content, type){
+	var alert = createAlert(content, type);
+	var collection = null;
+	if(type=='alert-info'){
+		collection = info;
+	}else if (type=='alert-danger'){
+		collection = danger;
+	}else if(type=='alert-success'){
+		collection = success;
+	}
+	var alertObj = {alert : alert, dismiss : function(){
+		collection.pop(info.indexOf(this));
+		if (info.length){
+			refreshAlerts(type, collection);
+		}else{
+			removeAlert(this.alert);
+		}
+	}};
+	collection.push(alertObj);
+	alertWrapper.appendChild(alert);
+	(function(alert, alertWrapper){
+		alert.getElementsByClassName('close')[0].addEventListener('click', function(){
+			removeAlert(alert);
+		})
+	})(alert, alertWrapper);
+	return alertObj;
+}
+
+function removeAlert(alert){
+	alert.classList.add('remove');
+	alert.addEventListener('webkitAnimationEnd', function(){
+		alertWrapper.removeChild(alert);
+	});
+}
+
+function getSelectedSnapshotName(){
+	var selectedSnapshot = document.querySelector('#UnattachedTable tr.selected');
+	return selectedSnapshot.getElementsByTagName('td')[1].innerHTML;
+}
+
+function handleSnapshotCreation(){
+	if(!attached){
+		attached = true;
+		var createSnapshotButton = document.querySelector('#create_snapshot_btn');
+		createSnapshotButton.addEventListener("click", function(){
+			var selectedSnapshotName = getSelectedSnapshotName();
+			setTimeout(function(){
+				var type='alert-info';
+				var existing = info.length;
+				var content = null;
+				if (existing){
+					var currInfo = document.querySelector('.alert-info');
+					removeAlert(currInfo);
+					content = createMultiInfoContent(info.length+1);
+				}else{
+					content = createSingleInfoContent(selectedSnapshotName);
+				}
+				var alertObj = addAlert(content, type);
+				setTimeout(function(){
+					alertObj.dismiss();
+				}, 20000);
+			},500);
+		});
+	}
+}
+
+if (parent === top){
+	window.addEventListener('load', function() {
+		container = document.querySelector('.data-row');
+		alertWrapper = document.createElement('div');
+		alertWrapper.classList.add('alertWrapper');
+		alertWrapper.classList.add('inline');
+		container.appendChild(alertWrapper);
+		var createSnapshotAction = document.querySelector('#CreateSnapshotItem');
+		createSnapshotAction.addEventListener('click', function(){
+			handleSnapshotCreation();
+		});
+	});
+}
+
+//console.log(window);
